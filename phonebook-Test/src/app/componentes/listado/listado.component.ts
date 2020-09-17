eb1df163f0d429e0df4a633c22fcf411f1827ba2
Kayoseo <juan_i_roman@hotmail.com>
2020-09-17 16:03:56 -0300

    Se agregan vista detalle y validador rut

--- phonebook-Test/src/app/componentes/listado/listado.component.ts
+++ phonebook-Test/src/app/componentes/listado/listado.component.ts
@@ -7,7 +7,8 @@ import { NzTableModule } from 'ng-zorro-antd/table';
 import { utf8Encode } from '@angular/compiler/src/util';
 import { Subscriber } from 'rxjs';
 import { FormControl } from '@angular/forms';
-
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
 
 @Component({
   selector: 'app-listado',
@@ -22,11 +23,20 @@ export class ListadoComponent implements OnInit {
   selectComuna: any;
   comunas: any;
   valueSearch: string;
-  public selectPersona: any;
+  selectPersona: any;
+  detallePersona: Persona;
+  rutValido: boolean;
+
+
+  isVisibleMiddle = false;
+
 
   constructor(private _personaService: PersonaService,
     private _regionService: RegionService) {
     this.comunas = [];
+    this.detallePersona = {};
+    this.rutValido = true;
+
   }
 
   ngOnInit() {
@@ -36,7 +46,6 @@ export class ListadoComponent implements OnInit {
     });
     this._regionService.get().subscribe(response => {
       this.regiones = response;
-      console.log(response);
     })
   }
 
@@ -73,6 +82,89 @@ export class ListadoComponent implements OnInit {
 
   }
 
+  showModalMiddle(persona): void {
+    console.log(persona);
+    this.detallePersona = persona;
+    this.checkRut(persona.rut);
+    if (this.detallePersona) {
+      this.isVisibleMiddle = true;
+    }
+
+  }
+  handleOkMiddle(): void {
+    console.log('click ok');
+    this.isVisibleMiddle = false;
+  }
+
+  handleCancelMiddle(): void {
+    this.isVisibleMiddle = false;
+  }
+
+  /* 
+  Validador de rut
+  */
+  ////basado en(algunas modificaciones): https://gist.github.com/rotvulpix/69a24cc199a4253d058c 
+  checkRut(rut) {
+    // Despejar Puntos
+    var valor = rut.replaceAll('.', '');
+    console.log('valor', valor);
+    // Despejar Guión
+    valor = valor.replace('-', '');
+
+    // Aislar Cuerpo y Dígito Verificador
+    let cuerpo = valor.slice(0, -1);
+    let dv = valor.slice(-1).toUpperCase();
+
+    // Formatear RUN
+    rut = cuerpo + '-' + dv
+
+    // Si no cumple con el mínimo ej. (n.nnn.nnn) 
+    //o Si excede el maximo de caracteres(nn.nnn.nnn)
+    if (cuerpo.length < 7 || cuerpo.length > 8) {
+      this.rutValido = false;
+      return false;
+    }
+
+
+
+    // Calcular Dígito Verificador
+    let suma = 0;
+    let multiplo = 2;
+
+    // Para cada dígito del Cuerpo
+    for (let i = 1; i <= cuerpo.length; i++) {
+
+      // Obtener su Producto con el Múltiplo Correspondiente
+      let index = multiplo * valor.charAt(cuerpo.length - i);
+
+      // Sumar al Contador General
+      suma = suma + index;
+
+      // Consolidar Múltiplo dentro del rango [2,7]
+      if (multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+
+    }
+
+    // Calcular Dígito Verificador en base al Módulo 11
+    let dvEsperado = 11 - (suma % 11);
+
+    // Casos Especiales (0 y K)
+    dv = (dv == 'K') ? 10 : dv;
+    dv = (dv == 0) ? 11 : dv;
+
+    // Validar que el Cuerpo coincide con su Dígito Verificador
+    if (dvEsperado != dv) {
+      this.rutValido = false;
+      return false;
+    }
+
+
+
+    // Si todo sale bien, eliminar errores (decretar que es válido)
+    console.log("rut valido")
+    this.rutValido = true;
+    return true;
+  }
 
 
 }
--- phonebook-Test/src/app/modelos/persona.ts
+++ phonebook-Test/src/app/modelos/persona.ts
@@ -1,30 +1,28 @@
 
 export class Persona {
 
-  public id: number;
-  public nombre: string;
-  public apellido: string;
-  public telefono: string;
-  public rut: string;
-  public direccion: {
-    calle: string;
-    numero: any;
-    comuna: {
-      id: number,
-      nombre: string
+  public id?: any;
+  public nombre?: any;
+  public apellido?: any;
+  public telefono?: any;
+  public rut?: any;
+  public direccion?: {
+    numero?: any;
+    comuna?: {
+      id?: any,
+      nombre?: any
     }
   };
-  public activo: number;
+  public activo?: number;
 
 
   constructor(id_persona, nombre, apellido, telefono, rut,
-    calle, numero, id_comuna, nombre_comuna, activo) {
+     numero, id_comuna, nombre_comuna, activo) {
     this.id = id_persona;
     this.nombre = nombre;
     this.apellido = apellido;
     this.telefono = telefono;
     this.rut = rut;
-    this.direccion.calle = calle;
     this.direccion.numero = numero;
     this.direccion.comuna.id = id_comuna;
     this.direccion.comuna.nombre = nombre_comuna;
